c6bb2411bf0095391b28c2204f20c4e1a797b6d9
oshea-patrick <oshea_patrick@hotmail.com>
2019-06-14 15:47:46 -0400

    Okay

--- R/dynlib.R
+++ R/dynlib.R
@@ -17,9 +17,19 @@ poThin <- function(df, spacing, dimension, lon, lat) {
 .Call('_RSpatial_poThin', PACKAGE = 'RSpatial', df, spacing, dimension, lon, lat)
 }
 
+#' classifySpatiallyByBlocks
+#' 
+#' This is also a Test
+#' 
+#' @param df
+#' @param lon
+#' @param lat
+#' @param size
+#' 
 #' @export
 #' @example 
 #' x=1
+#' 
 classifySpatiallyByBlocks <- function(df, lon, lat, size) {
-.Call('_RSpatial_classifySpatiallyByBlocks', PACKAGE = 'RSpatial', df, lon, lat, size)
+  .Call('_RSpatial_classifySpatiallyByBlocks', PACKAGE = 'RSpatial', df, lon, lat, size)
 }
