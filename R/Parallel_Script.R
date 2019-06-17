2702f03143ef75f4d445ea64bd800f413e5da7ff
oshea-patrick <oshea_patrick@hotmail.com>
2019-06-17 12:26:53 -0400

    Hopefully madepoThinP faster for larger datasets

--- R/Parallel_Script.R
+++ R/Parallel_Script.R
@@ -27,7 +27,6 @@ poThinP <- function(df, spacing, lon, lat, numCores) {
   lat1 = lat
   
   #break up into different lists of dataframes for cores
-  
   lists <- list()
   n=1
   for (i in unique(df$blocks)) {
@@ -35,6 +34,7 @@ poThinP <- function(df, spacing, lon, lat, numCores) {
     n = n+1
   }
   
+  # Clustering the cores and doring parallel calls
   parallel::clusterExport(cl,
                 c(
                  "spacing",
@@ -44,13 +44,36 @@ poThinP <- function(df, spacing, lon, lat, numCores) {
                 envir = environment())
 
   lists <- parallel::parLapply(cl, lists, poThinWrap)
-  parallel::stopCluster(cl)
   
+  # Thins the sub-dataframes
   unlisted <- unlist(lists)
   thinnedDf <- df[-unlisted, ]
-  
   thinnedDf <- thinnedDf[,c(lon, lat)]
   
-  return(thinnedDf[-poThin(df=thinnedDf, spacing = spacing, dimension = length(thinnedDf$lon), lon = lon, lat = lat), ])
+  blocks <- classifySpatiallyByBlocks(df=thinnedDf,lon = lon, lat = lat, size = length(thinnedDf$lon), blockSize = 20)
+  thinnedDf <- thinnedDf[,c(lon, lat)]
+  indices <- 1:length(thinnedDf$lon)
+  df <- cbind(df, blocks, indices)
+  colnames(thinnedDf) <- c(lon, lat, "blocks", "originalIndices")
+  
+  
+  lists <- list()
+  n=1
+  for (i in unique(thinnedDf$blocks)) {
+    lists[[n]] <- df[which(thinnedDf$blocks == i), ]
+    n = n+1
+  }
+  
+  lists <- parallel::parLapply(cl, lists, poThinWrap)
+  
+  # Thins the sub-dataframes
+  unlisted <- unlist(lists)
+  thinnedDf <- thinnedDf[-unlisted, ]
+  thinnedDf <- thinnedDf[,c(lon, lat)]
+  
+  
+  parallel::stopCluster(cl)
+  
+  return(thinnedDf)
 }
 
